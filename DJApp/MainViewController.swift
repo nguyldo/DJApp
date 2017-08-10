00cbe3bb9a6268a9d5ad2b16553f2ff69f6949ee
michaelxue <mike.xue7@gmail.com>
2017-08-09 17:41:55 -0700

    some change idk

---
+++ DJApp/MainViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MainViewController.swift
+//  DJApp
+//
+//  Created by Michael Xue on 7/17/17.
+//  Copyright © 2017 Gametime. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // [START auth_listener]
+        let Auth = new FIRAuth()
+        var handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            // [START_EXCLUDE]
+            self.setTitleDisplay(user)
+            self.tableView.reloadData()
+            // [END_EXCLUDE]
+        }
+        // [END auth_listener]
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // [START remove_auth_listener]
+        Auth.auth().removeStateDidChangeListener(handle!)
+        // [END remove_auth_listener]
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
